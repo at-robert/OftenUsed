d25d5247338b5cf3ecab9d08b751b16b66801d67
robert.lo <robert.lo@amtran.com.tw>
2021-08-13 00:54:07 +0800

    [import] - To adjust coding style

--- StockTW/stock_divined_date.py
+++ StockTW/stock_divined_date.py
@@ -49,26 +49,18 @@ def write_to_file(str_a):
         return 1
 
 
-def print_stock_divined_date(df_):
-    stock_id_list = ['2412','3045','2105','1229']
-
-    key0 = '代碼'
-    key1 = '股票名稱'
-    key2 = '股東會日期'
-    key3 = '除息日程'
-    key4 = '發放日'
+def print_stock_divined_date(df_,keys_):
+    stock_id_list = ['2412','3045','2105','1229','2489','2317']
 
     str_list = []
-
-    str_list.append(" {}  -- {} --- {} ".format(key1,key2,key3))
-    # print(" {}  -- {} --- {} ".format(key1,key2,key3))
+    str_list.append(" {}  ---- {} ---- {} ---- {} ".format(keys_[1],keys_[2],keys_[3],keys_[4]))
 
     for i in range(0, len(stock_id_list)):
-        stock_df = df_[df_[key0] == stock_id_list[i]]
+        stock_df = df_[df_[keys_[0]] == stock_id_list[i]]
         
         try:
-            if stock_df[[key0]].values[0] == stock_id_list[i]:
-                str_list.append(" {} - {} - {} - {} ".format(stock_df[[key1]].values[0],stock_df[[key2]].values[0],stock_df[[key3]].values[0],stock_df[[key4]].values[0]))
+            if stock_df[[keys_[0]]].values[0] == stock_id_list[i]:
+                str_list.append(" {} - {} - {} - {} ".format(stock_df[[keys_[1]]].values[0],stock_df[[keys_[2]]].values[0],stock_df[[keys_[3]]].values[0],stock_df[[keys_[4]]].values[0]))
                 # print(" {} - {} - {}".format(stock_df[[key1]].values[0],stock_df[[key2]].values[0],stock_df[[key3]].values[0]))
         except:
             print("Date Not available ",stock_id_list[i])
@@ -104,12 +96,14 @@ if __name__ == "__main__":
         # d = data.tolist()
         # arr.columns = d[0]
 
-        arr.columns = ['x','代碼', '股票名稱', '股東會日期','除息日程','x','x', 'x', '發放日','x','x','x', 'x', 'x','x','x','x', 'x', 'x','x']
+        keys_list = ['代碼','股票名稱','股東會日期','除息日程','發放日']
 
-        print(arr.columns)
-        df = arr[['代碼', '股票名稱', '股東會日期','除息日程','發放日']]
+        arr.columns = ['x',keys_list[0],keys_list[1],keys_list[2],keys_list[3],'x','x','x',keys_list[4],'x','x','x','x','x','x','x','x','x','x','x']
+
+        # print(arr.columns)
+        df = arr[keys_list]
         df = df.iloc[:,:5]
 
-        print_stock_divined_date(df)
+        print_stock_divined_date(df,keys_list)
     else:
         print("The file {} doesn't exist\n".format(filepath))
