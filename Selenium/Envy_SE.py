5f941be2090422fb2ac6ef71ef14e42c8e232d5e
robert.lo <robert.lo@amtran.com.tw>
2025-01-24 21:26:15 +0800

    [refactor] - To modify unwanted parts and improve functions of Envy

--- Selenium/Envy_SE.py
+++ Selenium/Envy_SE.py
@@ -10,6 +10,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 
+from selenium.webdriver.chrome.service import Service
 
 import time
 import re
@@ -42,9 +43,6 @@ def search_auth_file(folder, auth_data, pass_data, key_data):
 #----------------------------------------------------------------------
 if __name__ == "__main__":
 
-    chrome_options = Options()
-    chrome_options.add_experimental_option("detach", True)
-
     if len(sys.argv) < 2:
         print("用法：python Envy_SE.py <<target url>>")
         exit(1)  
@@ -58,25 +56,21 @@ if __name__ == "__main__":
 
     search_auth_file(pwd + 'Documents/CERT/auth_envy', auth_data, pass_data, key_data)
 
-    # binary = FirefoxBinary(pwd + 'firefox/firefox')
-    # print "path = %s" %(pwd + 'firefox')
-
-    # web = webdriver.Firefox(firefox_binary=binary)
-    # web = webdriver.Firefox()
     '''
     To control Chrome
     '''
-    web = webdriver.Chrome(pwd + 'chromedriver', chrome_options=chrome_options)
+    service = Service(executable_path= pwd + 'chromedriver')
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    web = webdriver.Chrome(service=service, options=options)
 
     web.get(url)
     web.set_window_position(0,0)
-    web.set_window_size(1442 + 610 ,1071)
-    # web.find_element_by_name("submit").click()
-    # web.find_element_by_link_text('登錄').click()
+    # web.set_window_size(1442 + 610 ,1071)
+    web.maximize_window()
     web.find_element("link text", "登錄").click()
     time.sleep(2)
 
-    # ids = web.find_elements_by_xpath('//*[@id]')
     ids = web.find_elements("xpath",'//*[@id]')
 
     for ii in ids: 
@@ -107,45 +101,8 @@ if __name__ == "__main__":
 
     print('loginanswer_row_' + id_ )
 
-    # web.find_element("id",'loginanswer_row_'+id_).clear()
-    # web.find_element("id",'loginanswer_row_'+id_).send_keys("Taipai")
-
     web.find_element("name",'answer').send_keys(key_data[0])
     web.find_element("name","loginsubmit").click()
-
-
-    # try:
-    #     web.find_element_by_link_text(u"[ 點擊這裡返回上一頁 ]").click()
-
-    #     time.sleep(2)
-    #     print ("Need to login again!!")
-    #     ids = web.find_elements_by_xpath('//*[@id]')
-
-    #     for ii in ids: 
-    #         if(re.search(r'^password.*',ii.get_attribute('id'))):
-    #             password_id = ii.get_attribute('id')
-    #             print (password_id)
-    #         elif(re.search(r'^username.*',ii.get_attribute('id'))):
-    #             username_id = ii.get_attribute('id')
-    #             print (username_id)
-
-    #     web.find_element_by_id(username_id).clear()
-    #     web.find_element_by_id(username_id).send_keys("robert_bt")
-    #     web.find_element_by_id(password_id).clear()
-    #     web.find_element_by_id(password_id).send_keys("070713")
-    #     web.find_element_by_name("loginsubmit").click()
-
-    #     time.sleep(3)
-    #     # web.find_element_by_link_text(u"成人電影(上傳空間)").click()
-    #     web.find_element("link text", u"成人電影(上傳空間)").click()
-    #     time.sleep(3)
-    #     # web.find_element_by_link_text(u"成人電影(上傳空間)").click()
-    #     web.find_element("link text", u"成人電影(上傳空間)").click()
-    #     time.sleep(6)
-    #     web.find_elements_by_xpath('//*[@id="wp"]/div[4]/table/tbody/tr[2]/td/table/tbody/tr/td[1]/form/input[2]').click()
-
-    # except: 
-    #     exit(1)
 
 
 
